f52407e616939537e65b5085683ab5cdaa678954
WolvarineXD <148878451+WolvarineXD@users.noreply.github.com>
2025-07-03 13:03:12 +0530

    Update jd_routes.py

--- routes/jd_routes.py
+++ routes/jd_routes.py
@@ -8,7 +8,7 @@ import httpx
 
 router = APIRouter(prefix="/jd", tags=["JD"])
 security = HTTPBearer()
-AI_ENDPOINT = "https://n8n-resume-ai.onrender.com/webhook-test/b2d90787-cbb4-448c-93c5-6a31eacfa99a"
+AI_ENDPOINT = "https://primary-production-60d91.up.railway.app/webhook-test/b2d90787-cbb4-448c-93c5-6a31eacfa99a"
 
 @router.post("/submit")
 async def submit_jd(
